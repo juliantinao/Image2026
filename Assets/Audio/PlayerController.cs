5de08d62d0e53df4956354dbcda3c48ebd621ca3
Julian Tinao <d.juliant@gmail.com>
2026-08-07 17:57:26 -0300

    jump

--- Assets/Audio/PlayerController.cs
+++ Assets/Audio/PlayerController.cs
@@ -3,10 +3,10 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    
+    private Rigidbody rb; 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -15,11 +15,15 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    void OnJump(InputValue value)
+    void OnJump()
     {
-        
-       Debug.Log("pressed");
-       
+        Debug.Log("pressed");
+        rb.AddForce(new Vector3(0,300,0));
 
+    }
+
+    void OnMove(InputAction input)
+    {
+        Debug.Log("moving");
     }
 }
